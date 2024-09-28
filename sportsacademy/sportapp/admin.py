0f4ee10b6eaaff2c362f040a19b6c677def5b857
Ravindra-RPM <menkudaleravindra@gmail.com>
2024-09-28 05:45:46 +0530

    Initial commit

--- sportsacademy/sportapp/admin.py
+++ sportsacademy/sportapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Coach, Team, Player
 
-# Register your models here.
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('uid', 'specialization')
+    search_fields = ('uid__username', 'specialization')
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'coach')
+    search_fields = ('name',)
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'age', 'team')
+    search_fields = ('first_name', 'last_name', 'team')
+
